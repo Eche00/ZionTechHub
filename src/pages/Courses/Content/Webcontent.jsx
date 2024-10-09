7b29302db117acda3b167d2eb4a7c320b2c1247e
Eche00 <echeeze956@gmail.com>
2024-10-09 14:00:43 +0100

    web content button

--- src/pages/Courses/Content/Webcontent.jsx
+++ src/pages/Courses/Content/Webcontent.jsx
@@ -23,13 +23,13 @@ function Webcontent() {
         <p className=" font-[600] text-[48px] text-[#333]">
           <span className=" text-[#034FE3]">Course </span>content
         </p>
-        <div className=" mt-[43px]   flex items-center justify-between bg-[#E9E9E9] rounded-full w-[597px]  ">
+        <div className=" mt-[43px]   flex items-center justify-between bg-[#E9E9E9] rounded-full md:w-[597px] w-[320px]  ">
           <button
             onClick={handleFrontEnd}
             className={
               frontEnd
-                ? " flex-1 bg-white rounded-full py-[16px] text-[#034FE3] whitespace-nowrap  text-[18px] font-[500]"
-                : " flex-1 text-[18px] font-[500] text-[#1A1A1ACC]"
+                ? " flex-1 bg-white rounded-full md:py-[16px] py-[10px] text-[#034FE3] whitespace-nowrap  md:text-[18px] font-[500]  text-[12px] md:px-0 px-[10px]"
+                : " flex-1 md:text-[18px] font-[500]  text-[12px] text-[#1A1A1ACC] whitespace-nowrap md:px-0 px-[10px]"
             }>
             Frontend course content
           </button>
@@ -37,8 +37,8 @@ function Webcontent() {
             onClick={handleBackEnd}
             className={
               backEnd
-                ? " flex-1 bg-white rounded-full py-[16px] text-[#034FE3] whitespace-nowrap  text-[18px] font-[500]"
-                : " flex-1 text-[18px] font-[500] text-[#1A1A1ACC]"
+                ? " flex-1 bg-white rounded-full md:py-[16px] py-[10px] text-[#034FE3] whitespace-nowrap  md:text-[18px] font-[500] text-[12px] md:px-0 px-[10px]"
+                : " flex-1 md:text-[18px] font-[500]  text-[12px] text-[#1A1A1ACC] whitespace-nowrap md:px-0 px-[10px]"
             }>
             Backend course content
           </button>
