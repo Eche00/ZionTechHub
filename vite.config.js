6146b4f3ce2f83c5abec1e3bdc38623d05e2cbda
Eche00 <echeeze956@gmail.com>
2025-05-01 23:26:10 +0100

    vite for redirects

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist", // output folder for the build
+//     emptyOutDir: true, // clear the output folder before building
+//   },
+//   // Define the public folder correctly
+//   publicDir: "public", // specify that Vite should use the /public directory for static files
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // output folder for the build
-    emptyOutDir: true, // clear the output folder before building
-  },
-  // Define the public folder correctly
-  publicDir: "public", // specify that Vite should use the /public directory for static files
 });
