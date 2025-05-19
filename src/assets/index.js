713b648b63465784bb1610a8172abd0bb914c5f0
Eche00 <echeeze956@gmail.com>
2025-05-19 18:31:10 +0100

    index.js name rename

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,163 +1,202 @@
-import Reviewimg from "./reviewimg.jpeg";
-import logo from "./logo.png";
+// logo
+import googlemeet from "./googlemeet.webp";
+
+// ---- Home Page images -----
+// Review
+import reviewimg1 from "./reviewimg1.webp";
+import reviewimg2 from "./reviewimg2.webp";
+import reviewimg3 from "./reviewimg3.webp";
+import reviewimg4 from "./reviewimg4.webp";
+import reviewimg5 from "./reviewimg5.webp";
+import reviewimg6 from "./reviewimg6.webp";
+import reviewimg7 from "./reviewimg7.webp";
+import reviewimg8 from "./reviewimg8.webp";
+
+// Ready made courses
+import Registerhome from "./Registerhome.webp";
+import Classeshome from "./Classeshome.webp";
+import Joinhome from "./Joinhome.webp";
+
+// Benefits
+import SirGodsentprofile from "./SirGodsentprofile.webp";
+import amazonlogo from "./amazonlogo.webp";
+import applelogo from "./applelogo.webp";
+import certificatehome from "./certificatehome.webp";
+import collabimagehome from "./collabimagehome.webp";
+import discord from "./discord.webp";
+import emojiprofile from "./emojiprofile.webp";
+import excel from "./excel.webp";
+import googleanalytics from "./googleanalytics.webp";
+import googlelogo from "./googlelogo.webp";
+import googlesheets from "./googlesheets.webp";
+import linkedlncover from "./linkedlncover.webp";
+import metalogo from "./metalogo.webp";
+import microsoft from "./microsoft.webp";
+import microsoftlogo from "./microsoftlogo.webp";
+import notionlogo from "./notionlogo.webp";
+import rstudio from "./rstudio.webp";
+import secondlogo from "./secondlogo.webp";
+import slack from "./slack.webp";
+import unilogo from "./unilogo.webp";
+import zapier from "./zapier.webp";
+
+// Testimonials
+import testimonial1 from "./testimonial1.webp";
+import testimonial2 from "./testimonial2.webp";
+import testimonial3 from "./testimonial3.webp";
+import testimonial4 from "./testimonial4.webp";
+import testimonial5 from "./testimonial5.webp";
+import testimonial6 from "./testimonial6.webp";
+import testimonial7 from "./testimonial7.webp";
+import testimonial8 from "./testimonial8.webp";
+import testimonial9 from "./testimonial9.webp";
+import testimonial10 from "./testimonial10.webp";
+import testimonial11 from "./testimonial11.webp";
+import testimonial12 from "./testimonial12.webp";
+import testimonial13 from "./testimonial13.webp";
+import testimonial14 from "./testimonial14.webp";
+import testimonial15 from "./testimonial15.webp";
+import testimonial16 from "./testimonial16.webp";
+import testimonial17 from "./testimonial17.webp";
+import testimonial18 from "./testimonial18.webp";
+
+// Popular courses
+import popularcourseimg1 from "./popularcourseimg1.webp";
+import popularcourseimg2 from "./popularcourseimg2.webp";
+import popularcourseimg3 from "./popularcourseimg3.webp";
+import popularcourseimg4 from "./popularcourseimg4.webp";
+
+// consultancy
+import consultancyhome from "./consultancyhome.webp";
+import consultafrica from "./consultafrica.webp";
+
+// ---------- About Page images ----------
+// hero section
+import abouthero1 from "./abouthero1.webp";
+import abouthero2 from "./abouthero2.webp";
+import abouthero3 from "./abouthero3.webp";
+
+// About us / Founders
+import Sirgodsentsecondprofile from "./Sirgodsentsecondprofile.webp";
+import abouthero4 from "./abouthero4.webp";
+
+// Our mission / vission
+import missionimage from "./missionimage.webp";
+import visionimage from "./visionimage.webp";
+
+// The team
+import teamprofile2 from "./teamprofile2.webp";
+import teamprofile3 from "./teamprofile3.webp";
+import teamprofile4 from "./teamprofile4.webp";
+import teamprofile5 from "./teamprofile5.webp";
+import teamprofile6 from "./teamprofile6.webp";
+import teamprofile7 from "./teamprofile7.webp";
+import teamprofile8 from "./teamprofile8.webp";
+import teamprofile9 from "./teamprofile9.webp";
+import teamprofile10 from "./teamprofile10.webp";
+
+// Partners
+import partnerlogo1 from "./partnerlogo1.webp";
+import partnerlogo2 from "./partnerlogo2.webp";
+import partnerlogo3 from "./partnerlogo3.webp";
+
+// Linkedln
+import linkedlnprofileimg from "./linkedlnprofileimg.webp";
+import mobile from "./mobile.webp";
+
+// ---------- Consultation Page ----------
+// her section
+import consulthero1 from "./consulthero1.webp";
+import consulthero2 from "./consulthero2.webp";
+import consulthero3 from "./consulthero3.webp";
+import consultprofile from "./consultprofile.webp";
+import usa from "./usa.webp";
+import uk from "./uk.webp";
+import nigeria from "./nigeria.webp";
+
+// Overview/Services
+import overviewimg from "./overviewimg.webp";
+import consultservice from "./consultservice.webp";
+import consultationpopup from "./consultationpopup.webp";
+
+// industries served
+import logo1 from "./logo1.webp";
+import logo2 from "./logo2.webp";
+import logo3 from "./logo3.webp";
+import logo4 from "./logo4.webp";
+import logo5 from "./logo5.webp";
+import logo6 from "./logo6.webp";
+import logo7 from "./logo7.webp";
+import logo8 from "./logo8.webp";
+import logo9 from "./logo9.webp";
+import logo10 from "./logo10.webp";
+import logo11 from "./logo11.webp";
+import logo12 from "./logo12.webp";
+
+// ---------- Partner with us Page ----------
+// hero section
+import partnerimg1 from "./partnerimg1.webp";
+import partnerimg2 from "./partnerimg2.webp";
+import partnerimg3 from "./partnerimg3.webp";
+
+// expertice
+import industryhero from "./industryhero.webp";
+
+// ---------- Courses ----------
+import analysticshero1 from "./analysticshero1.webp";
+import analysticshero2 from "./analysticshero2.webp";
+import analysticshero3 from "./analysticshero3.webp";
+import analysticshero4 from "./analysticshero4.webp";
+import analysticshero5 from "./analysticshero5.webp";
+import analysticshero6 from "./analysticshero6.webp";
+
+import sciencehero1 from "./sciencehero1.webp";
+import sciencehero2 from "./sciencehero2.webp";
+import sciencehero3 from "./sciencehero3.webp";
+import sciencehero4 from "./sciencehero4.webp";
+import sciencehero5 from "./sciencehero5.webp";
+import sciencehero6 from "./sciencehero6.webp";
+
+import webhero1 from "./webhero1.webp";
+import webhero2 from "./webhero2.webp";
+import webhero3 from "./webhero3.webp";
+import webhero4 from "./webhero4.webp";
+import webhero5 from "./webhero5.webp";
+import webhero6 from "./webhero6.webp";
+
+import cloudhero1 from "./cloudhero1.webp";
+import cloudhero2 from "./cloudhero2.webp";
+import cloudhero3 from "./cloudhero3.webp";
+import cloudhero4 from "./cloudhero4.webp";
+import cloudhero5 from "./cloudhero5.webp";
+import cloudhero6 from "./cloudhero6.webp";
+
+import machinehero1 from "./machinehero1.webp";
+import machinehero2 from "./machinehero2.webp";
+import machinehero3 from "./machinehero3.webp";
+import machinehero4 from "./machinehero4.webp";
+import machinehero5 from "./machinehero5.webp";
+import machinehero6 from "./machinehero6.webp";
+
+// ----- Certificates -----
+import certificate1 from "./certificate1.webp";
+import certificate2 from "./certificate2.webp";
+
+// ----- SVG's -----
 import individual from "./individual.svg";
 import enterprice from "./enterprice.svg";
 import startups from "./startups.svg";
-import Registerhome from "./Registerhome.png";
-import Classeshome from "./Classeshome.png";
-import Joinhome from "./Joinhome.png";
-import reviewimg1 from "./reviewimg1.png";
-import reviewimg2 from "./reviewimg2.png";
-import reviewimg3 from "./reviewimg3.png";
-import reviewimg4 from "./reviewimg4.png";
-import reviewimg5 from "./reviewimg5.png";
-import reviewimg6 from "./reviewimg6.png";
-import reviewimg7 from "./reviewimg7.png";
-import reviewimg8 from "./reviewimg8.png";
 
-import googlemeet from "./googlemeet.png";
-import certificatehome from "./certificatehome.png";
+// ----- Active -----
+import Reviewimg from "./reviewimg.jpeg";
+import logo from "./logo.png";
 import skillimagehome from "./skillimagehome.png";
-import collabimagehome from "./collabimagehome.png";
-import SirGodsentprofile from "./SirGodsentprofile.jpeg";
-import linkedlncover from "./linkedlncover.jpeg";
-import secondlogo from "./secondlogo.png";
-import unilogo from "./unilogo.png";
-import consultancyhome from "./consultancyhome.png";
-import consultafrica from "./consultafrica.png";
-import testimonial1 from "./testimonial1.jpeg";
-import testimonial2 from "./testimonial2.jpeg";
-import testimonial3 from "./testimonial3.jpeg";
-import testimonial4 from "./testimonial4.jpeg";
-import testimonial5 from "./testimonial5.jpeg";
-import testimonial6 from "./testimonial6.jpeg";
-import testimonial7 from "./testimonial7.jpeg";
-import testimonial8 from "./testimonial8.jpeg";
-import testimonial9 from "./testimonial9.jpeg";
-import testimonial10 from "./testimonial10.jpeg";
-import testimonial11 from "./testimonial11.jpeg";
-import testimonial12 from "./testimonial12.jpeg";
-import testimonial13 from "./testimonial13.jpeg";
-import testimonial14 from "./testimonial14.jpeg";
-import testimonial15 from "./testimonial15.jpeg";
-import testimonial16 from "./testimonial16.jpeg";
-import testimonial17 from "./testimonial17.jpeg";
-import testimonial18 from "./testimonial18.jpeg";
-import popularcourseimg1 from "./popularcourseimg1.jpeg";
-import popularcourseimg2 from "./popularcourseimg2.jpeg";
-import popularcourseimg3 from "./popularcourseimg3.jpeg";
-import popularcourseimg4 from "./popularcourseimg4.jpeg";
-import zapier from "./zapier.png";
-import slack from "./slack.png";
-import googleanalytics from "./googleanalytics.png";
-import rstudio from "./rstudio.png";
-import googlesheets from "./googlesheets.png";
-import excel from "./excel.png";
-import discord from "./discord.png";
-import microsoft from "./microsoft.png";
-import googlelogo from "./googlelogo.png";
-import metalogo from "./metalogo.png";
-import amazonlogo from "./amazonlogo.png";
-import notionlogo from "./notionlogo.png";
-import microsoftlogo from "./microsoftlogo.png";
-import applelogo from "./applelogo.png";
-import emojiprofile from "./emojiprofile.jpeg";
-import abouthero1 from "./abouthero1.png";
-import abouthero2 from "./abouthero2.png";
-import abouthero3 from "./abouthero3.png";
-import abouthero4 from "./abouthero4.png";
 import aboutfaqbg from "./aboutfaqbg.png";
-import mobile from "./mobile.png";
 import chiefoperator from "./chiefoperatorprofile.png";
-import missionimage from "./missionimage.jpeg";
-import visionimage from "./visionimage.png";
-import partnerlogo1 from "./partnerlogo1.jpeg";
-import partnerlogo2 from "./partnerlogo2.jpeg";
-import partnerlogo3 from "./partnerlogo3.jpeg";
-import Sirgodsentsecondprofile from "./Sirgodsentsecondprofile.jpeg";
 
-import certificate1 from "./certificate1.png";
-import certificate2 from "./certificate2.png";
-
-import teamprofile2 from "./teamprofile2.png";
-import teamprofile3 from "./teamprofile3.jpeg";
-import teamprofile4 from "./teamprofile4.jpeg";
-import teamprofile5 from "./teamprofile5.jpeg";
-import teamprofile6 from "./teamprofile6.jpeg";
-import teamprofile7 from "./teamprofile7.jpeg";
-import teamprofile8 from "./teamprofile8.jpeg";
-import teamprofile9 from "./teamprofile9.jpeg";
-import teamprofile10 from "./teamprofile10.jpeg";
-
-import analysticshero1 from "./analysticshero1.png";
-import analysticshero2 from "./analysticshero2.png";
-import analysticshero3 from "./analysticshero3.png";
-import analysticshero4 from "./analysticshero4.png";
-import analysticshero5 from "./analysticshero5.png";
-import analysticshero6 from "./analysticshero6.png";
-
-import sciencehero1 from "./sciencehero1.png";
-import sciencehero2 from "./sciencehero2.png";
-import sciencehero3 from "./sciencehero3.png";
-import sciencehero4 from "./sciencehero4.png";
-import sciencehero5 from "./sciencehero5.png";
-import sciencehero6 from "./sciencehero6.png";
-
-import webhero1 from "./webhero1.png";
-import webhero2 from "./webhero2.png";
-import webhero3 from "./webhero3.png";
-import webhero4 from "./webhero4.png";
-import webhero5 from "./webhero5.png";
-import webhero6 from "./webhero6.png";
-
-import cloudhero1 from "./cloudhero1.png";
-import cloudhero2 from "./cloudhero2.png";
-import cloudhero3 from "./cloudhero3.png";
-import cloudhero4 from "./cloudhero4.png";
-import cloudhero5 from "./cloudhero5.png";
-import cloudhero6 from "./cloudhero6.png";
-
-import machinehero1 from "./machinehero1.png";
-import machinehero2 from "./machinehero2.png";
-import machinehero3 from "./machinehero3.png";
-import machinehero4 from "./machinehero4.png";
-import machinehero5 from "./machinehero5.png";
-import machinehero6 from "./machinehero6.png";
-
-import consulthero1 from "./consulthero1.png";
-import consulthero2 from "./consulthero2.png";
-import consulthero3 from "./consulthero3.png";
-import consultservice from "./consultservice.png";
+// enroll
+import techhublogo from "./techhublogo.webp";
 import enrollbg from "./enrollbg.png";
-import techhublogo from "./techhublogo.jpeg";
-
-import consultprofile from "./consultprofile.jpeg";
-import usa from "./usa.png";
-import uk from "./uk.png";
-import nigeria from "./nigeria.png";
-
-import logo1 from "./logo1.png";
-import logo2 from "./logo2.png";
-import logo3 from "./logo3.png";
-import logo4 from "./logo4.png";
-import logo5 from "./logo5.png";
-import logo6 from "./logo6.png";
-import logo7 from "./logo7.png";
-import logo8 from "./logo8.png";
-import logo9 from "./logo9.png";
-import logo10 from "./logo10.png";
-import logo11 from "./logo11.png";
-import logo12 from "./logo12.png";
-
-import linkedlnprofileimg from "./linkedlnprofileimg.png";
-import overviewimg from "./overviewimg.jpeg";
-
-import partnerimg1 from "./partnerimg1.jpeg";
-import partnerimg2 from "./partnerimg2.png";
-import partnerimg3 from "./partnerimg3.png";
-
-import industryhero from "./industryhero.png";
-import consultationpopup from "./consultationpopup.png";
 
 export {
   logo,
