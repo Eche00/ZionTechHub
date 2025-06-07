d258c59089ea1c1b07992ec1d50f8ddd7ef0b77d
Eche00 <echeeze956@gmail.com>
2025-06-07 16:11:53 +0100

    adding attendee count

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -75,7 +75,8 @@ function Preview() {
           </div>
         ))}
       </div>
-      <div className="flex justify-center mb-6">
+      <p className="my-6 text-center">Attendes: {attendees.length}</p>
+      <div className="flex justify-center my-6 ">
         <button
           onClick={downloadPDF}
           className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
