40e015adfeb3e32777a786f89bc19e9ba8ddfc6c
Eche00 <echeeze956@gmail.com>
2025-06-24 01:43:48 +0100

    registeration logic

---
+++ src/lib/regLogic.js
@@ -0,0 +1,30 @@
+// regLogic to handle the registration process
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "./Config/firebase";
+
+export const handleRegistration = async (formData) => {
+  const { username, email, password, role, createdAt } = formData;
+
+  try {
+    // create User  in firebase
+    const userCresidentials = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCresidentials.user;
+
+    // Store additional user details in Firestore
+    await setDoc(doc(db, "users", user.uid), {
+      username,
+      email,
+      role,
+      createdAt,
+    });
+    console.log("User registered successfully:");
+  } catch (error) {
+    console.error("Registeration  failed:", error);
+    throw new Error("Registeration  Failed. Please try again !!");
+  }
+};
