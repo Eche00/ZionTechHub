8f9e57f16227bd454479490f3f280e682de1d3d2
Eche00 <echeeze956@gmail.com>
2025-06-26 23:06:29 +0100

    Dashboard page

--- src/dashboard/dashboardRoutes/Dashboard.jsx
+++ src/dashboard/dashboardRoutes/Dashboard.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Chart from "../dashboardcomponents/Chart";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="h-fit pt-10">
+      <div className="flex items-center gap-[20px] border-b-2 border-gray-700 py-[10px]">
+        <h1 className="text-3xl font-bold text-white">Dashboard</h1>
+        <Link
+          to="/dashboard/viewblogs"
+          className="bg-transparent text-gray-500 border-2 border-gray-700 px-4 py-2 rounded-full hover:scale-[102%] transition">
+          View Blogs
+        </Link>
+        <button className="bg-transparent text-gray-500 border-2 border-gray-700 px-4 py-2 rounded-full cursor-default">
+          1 Image per Blog / Title = Meta title
+        </button>
+      </div>
+      {/* main  */}
+
+      <main className=" flex flex-col gap-10 w-full pb-[400px] mt-10 ">
+        <section className="flex flex-1  gap-2  border-2 border-gray-700  w-full px-3 py-4 rounded-[10px] text-white ">
+          <Chart />
+        </section>
+        {/* bottom section  */}
+        <section className=" flex w-full  gap-10">
+          <div className="flex flex-1  gap-2  border-4 border-gray-700  w-full px-3 py-4 rounded-[10px]  "></div>
+          <div className="flex flex-1  gap-2  border-4 border-gray-700  w-full px-3 py-4 rounded-[10px]  "></div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default Dashboard;
