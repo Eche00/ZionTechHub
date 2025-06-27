1ce001fa269164cc9c6540b8d1ee4db898eef817
Eche00 <echeeze956@gmail.com>
2025-06-27 23:20:30 +0100

    Dashboard ui edit

--- src/dashboard/dashboardRoutes/Dashboard.jsx
+++ src/dashboard/dashboardRoutes/Dashboard.jsx
@@ -2,32 +2,53 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Chart from "../dashboardcomponents/Chart";
 import Links from "../dashboardcomponents/Links";
+import Recent from "../dashboardcomponents/Recent";
 
 function Dashboard() {
   return (
-    <div className="h-fit pt-10">
+    <div className="h-[100vh] pt-10 overflow-scroll">
       <div className="flex items-center gap-[20px] border-b-2 border-gray-700 py-[10px]">
         <h1 className="text-3xl font-bold text-white">Dashboard</h1>
         <Link
           to="/dashboard/viewblogs"
-          className="bg-transparent text-gray-500 border-2 border-gray-700 px-4 py-2 rounded-full hover:scale-[102%] transition">
+          className="bg-transparent text-gray-500 border-2 border-gray-700 px-4 py-2 rounded-full w-[140px] text-nowrap flex items-center justify-center hover:scale-[105%] transition-all duration-300 cursor-pointer">
           View Blogs
         </Link>
-        <button className="bg-transparent text-gray-500 border-2 border-gray-700 px-4 py-2 rounded-full cursor-default">
-          1 Image per Blog / Title = Meta title
-        </button>
+        <Link
+          to="/dashboard/create-blog"
+          className="bg-transparent text-gray-500 border-2 border-gray-700 px-4 py-2 rounded-full w-[140px] text-nowrap flex items-center justify-center hover:scale-[105%] transition-all duration-300 cursor-pointer">
+          Create Blogs
+        </Link>
+        <Link
+          to="/dashboard/event-attendees"
+          className="bg-transparent text-gray-500 border-2 border-gray-700 px-4 py-2 rounded-full w-[140px] text-nowrap flex items-center justify-center hover:scale-[105%] transition-all duration-300 cursor-pointer">
+          Event attendees
+        </Link>
+        <Link
+          to="/dashboard/create-webinar"
+          className="bg-transparent text-gray-500 border-2 border-gray-700 px-4 py-2 rounded-full w-[140px] text-nowrap flex items-center justify-center hover:scale-[105%] transition-all duration-300 cursor-pointer">
+          Webinar/workshop
+        </Link>
+        <Link
+          to="/dashboard/users"
+          className="bg-transparent text-gray-500 border-2 border-gray-700 px-4 py-2 rounded-full w-[140px] text-nowrap flex items-center justify-center hover:scale-[105%] transition-all duration-300 cursor-pointer">
+          Users
+        </Link>
       </div>
       {/* main  */}
 
-      <main className=" flex flex-col gap-10 w-full pb-[400px] mt-10 ">
-        <section className="flex flex-1  gap-2  border-2 border-gray-700  w-full px-3 py-4 rounded-[10px] text-white ">
+      <main className=" flex flex-col gap-5 w-full pb-[200px] mt-10 ">
+        <section className="flex flex-1  gap-2  border-4 border-gray-700  w-full px-3 py-4 rounded-[10px] text-white ">
           <Chart />
         </section>
         {/* bottom section  */}
         <section className=" flex w-full  gap-10">
-          <div className="flex flex-1  gap-2  border-4 border-gray-700  w-full px-3 py-4 rounded-[10px]  "></div>
+          {/* Recent  */}
+          <div className="flex flex-1  gap-2  border-4 border-gray-700  w-full px-3 py-4 rounded-[10px] overflow-hidden">
+            <Recent />
+          </div>
           {/* link  */}
-          <div className="flex w-[40%] gap-2  border-4 border-gray-700  px-3 py-4 rounded-[10px]  ">
+          <div className="flex w-[30%] gap-2  border-4 border-gray-700  px-3 py-4 rounded-[10px]  ">
             <Links />
           </div>
         </section>
